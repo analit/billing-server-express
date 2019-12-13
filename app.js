ff04671e4359d7a1305aee3ddc6787c13a5d7865
serg <s.garyaga@dzento.com>
2019-12-13 11:24:39 +0200

    12.12

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
+
+// mongodb+srv://serg:<password>@cluster0-bvxuh.mongodb.net/test?retryWrites=true&w=majority
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const billingRouter = require('./routes/billing')
 const apiRouter = require('./routes/api')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
